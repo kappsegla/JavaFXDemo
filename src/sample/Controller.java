9cbdadaf8a68c3b5757f9c4a15235edd010a79d7
Martin Blomberg <martin.blomberg@outlook.com>
2018-10-02 14:11:03 +0200

    Fxml view file updated. Button onAction added.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    public void button1Action(ActionEvent actionEvent) {
+        ((Button) actionEvent.getSource()).setText("Clicked");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,13 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
