0dbfbca2d24ce2df3223696e44aceb58819a5b1a
Martin Blomberg <martin.blomberg@outlook.com>
2018-10-03 13:56:07 +0200

    ObservableList

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,16 @@
 package sample;
 
+import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class Controller {
 
@@ -27,7 +33,7 @@ public class Controller {
 
     public void button1Action(ActionEvent actionEvent) {
         model.setText("Button 1 was just pressed");
-        model.setDisabled(!model.getDisabled());
+        model.setDisabled(!model.isDisabled());
     }
 
     public void button2Action(ActionEvent actionEvent) {
@@ -39,5 +45,28 @@ public class Controller {
         model.textProperty().bindBidirectional(textField1.textProperty());
         model.textProperty().bindBidirectional(textField2.textProperty());
         button2.disableProperty().bind(model.disabledProperty());
+
+        model.getObservableList().addListener((ListChangeListener<Point2D>) c -> draw());
     }
+
+    public void canvasClicked(MouseEvent event) {
+        Point2D point = new Point2D(event.getX(), event.getY());
+        model.getObservableList().add(point);
+    }
+
+    public void draw() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.WHITE);
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        Paint p = Color.RED;
+        gc.setFill(p);
+        for (Point2D point : model.getObservableList()) {
+            gc.fillOval(point.getX() - 10, point.getY() - 10, 20, 20);
+        }
+        gc.setLineWidth(5.0);
+        gc.strokeRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    }
+
 }
--- src/sample/Model.java
+++ src/sample/Model.java
@@ -4,13 +4,30 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Model {
 
     private StringProperty text;
     private BooleanProperty disabled;
+    private List<Point2D> points = new ArrayList<>();
+    private ObservableList<Point2D> observableList =
+            FXCollections.observableList(points);
 
-    public boolean getDisabled() {
+    public ObservableList<Point2D> getObservableList() {
+        return observableList;
+    }
+
+    public void setObservableList(ObservableList<Point2D> observableList) {
+        this.observableList = observableList;
+    }
+
+    public boolean isDisabled() {
         return disabled.get();
     }
 
